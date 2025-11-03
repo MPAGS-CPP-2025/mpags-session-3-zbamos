e3f93e9b8aaebca4023f42f2809cdb511fd06779
Zack Amos <dk19435@bristol.ac.uk>
2025-11-03 14:45:08 +0000

    end of the intorducing c++ part

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -4,6 +4,20 @@ int main()
     // This is a comment
     /* This comment
     is Mulitline! */
-    std::cout << "Hello, World!\n";
+    constexpr int a{4};          // bad!
+    constexpr double b{1.2*a}; 
+    double c{3.4};
+
+    //a = 43;
+    //b = 2.2;
+
+    c = a*b;
+
+    std::cout << c << std::endl;
+   
+    std::string msg {"Hello"};
+
+    std::cout << msg[4] <<  "\n";
+
     return 0;
 }
